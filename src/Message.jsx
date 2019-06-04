49e66d0dc564747585ea33a042432271771e0b1d
Julia Moses <jslmoses@gmail.com>
2019-06-04 15:13:00 -0400

    initial state set, curent user set with props

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,30 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 class Message extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {currentUser: {name: "Bob"}
-                  Messages:
-                   };
-    } 
-	render() {
-    return (
-    	<div>
-    	 <div className="message">
-    			<span className="message-username">Anonymous1</span>
-   			  <span className="message-content">I won't be impressed with technology until I can download food.</span>
-  			</div>
- 			  <div className="message system">
-    			Anonymous1 changed their name to nomnom.
-  			</div></div>
-    );
 
-      this.state = {
+    
 
-      }
 
-  }
+    render() {
+        return (
+            <div>
+       <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">
+          I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div></div>
+        );
+    }
 
 }
 
