b1f6b929da1ab73c49be5710b091046243bae3e7
Julia Moses <jslmoses@gmail.com>
2019-06-05 09:41:01 -0400

    web socket server for chatty app created

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,25 +1,6 @@
 import React, { Component } from 'react';
 
-
-
 class ChatBar extends Component {
-	// handleInput = e => {
-	// 	const {
-	// 		userHitsEnter
-	// 	} = this.props;
-
-
-
-	// 	if (e.key === 'Enter') {
-	// 		userHitsEnter({
-	// 			content: e.target.value,
-	// 			type: e.target.name === 'messageInput' ? 'messageSent' 
-	// 				: e.target.name === 'userInput' ? 'userNameChanged' 
-	// 				: '',
-	// 		})
-	// 	}
-	// }
-
   render() {
     return (
     	<form className="chatbar">
